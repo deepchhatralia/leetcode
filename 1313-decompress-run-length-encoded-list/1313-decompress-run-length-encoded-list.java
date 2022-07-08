fa6c0f0e2a6b744bab67d4c23659716a519eeaea
deepchhatralia <84608736+deepchhatralia@users.noreply.github.com>
2022-07-08 21:39:04 +0530

    Time: 7 ms (27.85%), Space: 49.3 MB (11.04%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[] decompressRLElist(int[] nums) {
-        int counter = -1;
         List<Integer> lst = new ArrayList<Integer>();
         
         for(int i = 0 ; i < nums.length ; i+=2){
             for(int j = 0 ; j < nums[i] ; j++){
                 lst.add(nums[i+1]);
-                // arr[++counter] = nums[i+1];
             }
         }
         
