b45883cf6098019219a5bbc4e7b680335e95e9ee
deepchhatralia <84608736+deepchhatralia@users.noreply.github.com>
2022-07-08 21:57:09 +0530

    Time: 2 ms (60.88%), Space: 43.9 MB (81.82%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char ch[] = new char[s.length()];
+        
+        for(int i = 0 ; i < indices.length ; i++){
+            ch[indices[i]] = s.charAt(i);
+        }
+        String str = new String(ch);
+        
+        return str;
+    }
+}
