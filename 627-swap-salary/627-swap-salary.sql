574245c7de82e9336d751ac51304c2075a7e57dc
deepchhatralia <84608736+deepchhatralia@users.noreply.github.com>
2022-07-13 08:09:33 +0530

    Time: 479 ms (13.79%), Space: 0B (100.00%) - LeetHub

--- 627-swap-salary/627-swap-salary.sql
+++ 627-swap-salary/627-swap-salary.sql
@@ -1,2 +1,8 @@
+# update Salary
+# set sex = if(sex='m','f','m')
+
 update Salary
-set sex = if(sex='m','f','m')
+set sex = CASE sex
+            WHEN 'm' THEN 'f'
+            ELSE 'm'
+        END
