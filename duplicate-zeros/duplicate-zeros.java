9914293c76da9a565038c5b640457168c8e90c29
deepchhatralia <84608736+deepchhatralia@users.noreply.github.com>
2023-02-12 16:34:42 +0530

    Time: 12 ms, Memory: 42.3 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.java
+++ duplicate-zeros/duplicate-zeros.java
@@ -3,12 +3,16 @@ class Solution {
         int temp = -1;
         
         for(int i = 0 ; i < arr.length ; i++) {
+            
             if(arr[i] == 0 && temp != i){
+                
                 for(int j = arr.length - 1 ; j > i ; j--){
                     arr[j] = arr[j - 1];
                 }
+                
                 temp = i + 1;
             }
+            
         }
     }
 }
