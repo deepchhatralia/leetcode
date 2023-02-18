5264c30b41f5f053562187a6431b35efdc619482
deepchhatralia <84608736+deepchhatralia@users.noreply.github.com>
2023-02-18 17:58:26 +0530

    Time: 3 ms, Memory: 45.1 MB - LeetHub

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -14,16 +14,22 @@ class Solution {
         int max = Integer.MIN_VALUE;
         
         for(int i = arr.length - 1 ; i >= 0 ; i--){
+            // if index is last than replace it with -1
             if(i == arr.length - 1){
                 max = Math.max(max,arr[i]);
                 arr[i] = -1;
                 continue;
             }
+            
             max = Math.max(max,arr[i + 1]);
             int temp = max;
             max = arr[i];
             arr[i] = temp;
         }
+        
+        //        17     18     5    4      6      1
+        // arr[i] 18     6      6    6      1     -1
+        // max    (6,18) (6,5) (6,4) (1,6) (1,-1) -1
         
         return arr;
     }
