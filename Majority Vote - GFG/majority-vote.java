c0f6655b43972fd2c56d780a6c96c53e87f8dace
deepchhatralia <84608736+deepchhatralia@users.noreply.github.com>
2023-04-14 16:48:20 +0530

    Added solution - LeetHub

--- Majority Vote - GFG/majority-vote.java
+++ Majority Vote - GFG/majority-vote.java
@@ -75,16 +75,52 @@ class Solution {
         // }
         
         
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        // HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        // for(int i = 0 ; i < n ; i++){
+        //     hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
+        // }
+        
+        // for (int key : hm.keySet()) {
+        //     if(hm.get(key) > (n / 3))
+        //         list.add(key);
+        // }
+        
+        
+        
+        int c1 = 0, c2 = 0, e1 = 0, e2 = 0;
         
         for(int i = 0 ; i < n ; i++){
-            hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
+            if(c1 == 0 && e2 != arr[i]){
+                c1 = 1;
+                e1 = arr[i];
+            }else if(c2 == 0 && e1 != arr[i]){
+                c2 = 1;
+                e2 = arr[i];
+            }else if(arr[i] == e1){
+                c1++;
+            }else if(arr[i] == e2){
+                c2++;
+            }else{
+                c1--;
+                c2--;
+            }
+        }
+        c1 = 0; c2 = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == e1)
+                c1++;
+            if(arr[i] == e2)
+                c2++;
         }
         
-        for (int key : hm.keySet()) {
-            if(hm.get(key) > (n / 3))
-                list.add(key);
-        }
+        if(c1 > (n / 3))
+            list.add(e1);
+        if(c2 > (n / 3))
+            list.add(e2);
+            
         
         
         
