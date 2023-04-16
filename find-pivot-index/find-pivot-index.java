5c413b52b80a7bc1304f38773f62427ea0501f42
deepchhatralia <84608736+deepchhatralia@users.noreply.github.com>
2023-04-16 20:53:56 +0530

    Time: 1 ms, Memory: 43.7 MB - LeetHub

--- find-pivot-index/find-pivot-index.java
+++ find-pivot-index/find-pivot-index.java
@@ -1,22 +1,34 @@
 class Solution {
-    // [1,7,3,6,5,6]
-
     public int pivotIndex(int[] arr) {
-        int rightSum[] = new int[arr.length];
-        rightSum[arr.length-1] = arr[arr.length-1];
+//         int rightSum[] = new int[arr.length];
+//         rightSum[arr.length-1] = arr[arr.length-1];
         
-        int leftSum[] = new int[arr.length];
-        leftSum[0] = arr[0];
+//         int leftSum[] = new int[arr.length];
+//         leftSum[0] = arr[0];
         
-        for(int i = 1 ; i < arr.length ; i++){
-            leftSum[i] = leftSum[i-1] + arr[i];
+//         for(int i = 1 ; i < arr.length ; i++){
+//             leftSum[i] = leftSum[i-1] + arr[i];
+//         }
+//         for(int i = arr.length-2 ; i >= 0 ; i--){
+//             rightSum[i] = rightSum[i+1] + arr[i];
+//         }
+//         for(int i = 0 ; i < arr.length ; i++){
+//             if(leftSum[i] == rightSum[i])
+//                 return i;
+//         }
+//         return -1;
+        
+        int leftSum = 0, sum = 0;
+        
+        for(int x : arr){
+            sum += x;
         }
-        for(int i = arr.length-2 ; i >= 0 ; i--){
-            rightSum[i] = rightSum[i+1] + arr[i];
-        }
+        
         for(int i = 0 ; i < arr.length ; i++){
-            if(leftSum[i] == rightSum[i])
+            if(leftSum == (sum - leftSum - arr[i]))
                 return i;
+            
+            leftSum += arr[i];
         }
         return -1;
     }
