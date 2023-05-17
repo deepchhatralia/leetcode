13a4a724925c70988beec4f5ee5508b57ca2e446
Deep Chhatralia <deepchhatralia77@gmail.com>
2023-05-17 16:46:07 +0530

    Time: 7 ms (13.34%), Space: 43.5 MB (80.88%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,25 +11,39 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        // tortoise approach
         if(head == null || head.next == null)
             return false;
         
         if(head.next.next == head)
             return true;
         
-        ListNode slow = head;
-        ListNode fast = head;
-        int flag = 1;
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         int flag = 1;
         
-        while(flag == 1 || slow != fast){
-            if(fast == null || fast.next == null)
-                return false;
+//         while(flag == 1 || slow != fast){
+//             if(fast == null || fast.next == null)
+//                 return false;
             
-            flag = 0;
-            slow = slow.next;
-            fast = fast.next.next;
+//             flag = 0;
+//             slow = slow.next;
+//             fast = fast.next.next;
             
+//         }
+//         return true;
+        
+        
+        HashMap<ListNode,Integer> hm = new HashMap<>();
+        ListNode index = head;
+        
+        while(index.next != null){
+            if(hm.getOrDefault(index,0) != 0)
+                return true;
+            
+            hm.put(index, 1);
+            index = index.next;
         }
-        return true;
+        return false;
     }
 }
