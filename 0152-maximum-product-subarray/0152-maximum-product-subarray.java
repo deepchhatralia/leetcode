31304f0a6dd9ee38270b17ede8228b89f0408fd4
Deep Chhatralia <deepchhatralia77@gmail.com>
2023-05-21 15:38:00 +0530

    Time: 1 ms (87.06%), Space: 43.8 MB (6.87%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,13 +3,28 @@ class Solution {
         int max = Integer.MIN_VALUE;
         int len = nums.length;
         
+        // for(int i = 0 ; i < len ; i++){
+        //     int sum = 1;
+        //     for(int j = i ; j < len ; j++){
+        //         sum *= nums[j];
+        //         max = Math.max(max, sum);
+        //     }
+        // }
+        
+        
+        
+        int prefix = 1, suffix = 1;
+        
         for(int i = 0 ; i < len ; i++){
-            int sum = 1;
-            for(int j = i ; j < len ; j++){
-                sum *= nums[j];
-                max = Math.max(max, sum);
-            }
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+            
+            prefix *= nums[i];
+            suffix *= nums[len - i - 1];
+            
+            max = Math.max(max, Math.max(prefix, suffix));
         }
+        
         return max;
     }
 }
