f97199ea1ff1e9e3b1552d9d268692b91013f7c5
Deep Chhatralia <deepchhatralia77@gmail.com>
2023-05-23 13:50:59 +0530

    Time: 0 ms (100.00%), Space: 41 MB (27.84%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -0,0 +1,80 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ListNode mainHead;
+    
+//     public int getSize(ListNode head){
+//         ListNode index = head;
+//         int count = 0;
+        
+//         while(index != null){
+//             count++;
+//             index = index.next;
+//         }
+//         return count;
+//     }
+    
+    public ListNode removeNthFromEnd(ListNode head, int n){
+        if(head.next == null){
+            head = null;
+            return head;
+        }
+        if(head.next.next == null){
+            if(n == 1)
+                head.next = null;
+            else
+                head = head.next;
+            
+            return head;
+        }
+        
+//         int count = 0, size = getSize(head);
+//         if(size == n){
+//             head = head.next;
+//             return head;
+//         }
+        
+//         ListNode prev = null;
+//         ListNode curr = head;
+        
+//         while(size-count != n){
+//             count++; // 1 2 3
+//             prev = curr; // 1 2 3
+//             curr = curr.next; // 2 3 4
+//         }
+//         prev.next = curr.next;
+//         return head;
+        
+        
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        dummy.val = head.val;
+        
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        
+        for(int i = 1 ; i <= n ; i++)
+            fast = fast.next;
+        
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        if(slow == dummy){
+            return head.next;
+        }
+        
+        slow.next = slow.next.next;
+        
+        return head;
+    }
+}
