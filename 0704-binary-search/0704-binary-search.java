ef4efdee10be1ab5b18586c59999c594c96bd766
Deep Chhatralia <deepchhatralia77@gmail.com>
2023-07-22 11:31:01 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (69.84%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int bs(int arr[], int left, int right, int target){
+        if(left > right)
+            return -1;
+        
+        int mid = (left + right) / 2;
+        
+        if(arr[mid] == target)
+            return mid;
+        
+        if(arr[mid] < target)
+            return bs(arr, mid + 1, right, target);
+        else
+            return bs(arr, left, mid - 1, target);
+    }
+    
+    public int search(int[] arr, int k) {
+        return bs(arr,0, arr.length - 1, k);
+//         if(arr.length == 1 && arr[0] == k) return 0;
+
+//         int n = arr.length;
+//         int left = 0,mid = n/2 , right = n-1;
+        
+//         while(left <= right){
+//             if(arr[mid] == k)
+//                 return mid;
+//             else if(k < arr[mid])
+//                 right = mid - 1;
+//             else 
+//                 left = mid + 1;
+                
+//             mid = (left + right) / 2;
+//         }
+        
+//         return -1;
+    }
+}
